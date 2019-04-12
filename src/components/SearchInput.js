d66352538fd6c4462c4757a581f37096e12dd85f
Pedro Neto <pedro.diogo.neto@outlook.pt>
2019-04-12 11:16:55 +0200

    Add visualFeedback to SearchInput state

---
+++ src/components/SearchInput.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+export default function SearchButton(props) {
+    const [inputNumber, setInputNumber] = useState('')
+    const [validationStatus, setValidationStatus] = useState('')
+    const [visualFeedback, setVisualFeedback] = useState(undefined)
+
+    function handleOnChangeInputNumber(e) {
+        let integerRegex =/^\d+$/;
+        console.log(integerRegex.test(e))
+        if(integerRegex.test(e)) {
+            setVisualFeedback(undefined)
+            setInputNumber(e)
+        } else {
+            setVisualFeedback(undefined)
+        }
+    }
+
+
+    return (
+        <React.Fragment>
+            
+            <form type="submit">
+                <input type="number" value={inputNumber} onChange={e => handleOnChangeInputNumber(e.target.value)}/>
+                <button onClick={() => props.onSubmitInput(inputNumber)}>Search!</button>
+            </form>
+            {visualFeedback?
+                <div>
+
+                </div>
+                : null
+            }
+        </React.Fragment>
+    );
+}
+
+const styles = {
+
+}
